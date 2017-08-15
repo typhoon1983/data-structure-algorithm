0095e471c1b17a55389cb8530b1ba6874db84293
Yu Tian <yu.tian113@gmail.com>
2017-08-15 21:30:06 +1000

    Sequential List testing and comments(TBD)

--- List_Sequential.js
+++ List_Sequential.js
@@ -1,38 +1,64 @@
 class SeqList {
-  constructor(elements, maxSize) {
+
+  /*
+   *  Constructor, generate the SeqList from the arguments passed in
+   */
+  constructor(elements = [], maxSize = 0) {
     this.maxSize = maxSize;
     this.elements = elements.slice(0,maxSize);
   }
 
+  /*
+   *  Empty the SeqList
+   */
   clearList(){
     // this.elements is the only place referencing the original array which will be Garbage Collected after being replaced by a new empty array
     this.elements = [];
+    return this;
   }
 
+  /*
+   *  Getter, elements length of the SeqList
+   */
   get length(){
     return this.elements.length;
   }
 
+  /*
+   *  Get element by specify index(1-based)
+   */
   getElement(i){
     i--;
     return i >= 0 && i < this.elements.length ? this.elements[i] : null;
   }
 
+  /*
+   *  Return element(first appearance) index(1-based) by specify a value
+   */
   findElement(x){
     let index = this.elements.indexOf(x);
-    return index > -1 ? index : null;
+    return ++index > 0 ? index : null;
   }
 
-  get priorElement(i){
+  /*
+   *  Return element before the specified index(1-based)
+   */
+  getPrevElement(i){
     i--;
-    return i > 0 ? this.elements[i-1] : null;
+    return i > 0 && i < this.elements.length ? this.elements[i-1] : null;
   }
 
-  get nextElement(i){
+  /*
+   *  Return element after the specified index(1-based)
+   */
+  getNextElement(i){
     i--;
-    return i < this.elements.length - 1 ? this.elements[i+1] : null;
+    return i >= 0 && i < this.elements.length - 1 ? this.elements[i+1] : null;
   }
 
+  /*
+   *  Insert element at the specified index(1-based)
+   */
   insertElement(x,i){
     i--;
     if(i < 0 || i > this.elements.length || this.elements.length === this.maxSize){
@@ -42,6 +68,9 @@ class SeqList {
     return this.elements;
   }
 
+  /*
+   *  Delete element at the specified index(1-based)
+   */
   deleteElement(i){
     i--;
     if(i < 0 || i > this.elements.length - 1){
